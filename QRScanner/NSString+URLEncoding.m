b6d9db8bbd03cd9e52cc861960b58a937b916309
Chris Dzombak <chris@chrisdzombak.net>
2013-10-27 23:03:28 -0400

    Escape spaces as %20

--- QRScanner/NSString+URLEncoding.m
+++ QRScanner/NSString+URLEncoding.m
@@ -17,9 +17,7 @@
     unsigned long sourceLen = strlen((const char *)source);
     for (unsigned long i = 0; i < sourceLen; ++i) {
         const unsigned char thisChar = source[i];
-        if (thisChar == ' '){
-            [output appendString:@"+"];
-        } else if (thisChar == '.' || thisChar == '-' || thisChar == '_' || thisChar == '~' ||
+        if (thisChar == '.' || thisChar == '-' || thisChar == '_' || thisChar == '~' ||
                    (thisChar >= 'a' && thisChar <= 'z') ||
                    (thisChar >= 'A' && thisChar <= 'Z') ||
                    (thisChar >= '0' && thisChar <= '9')) {
