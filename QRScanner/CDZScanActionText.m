5e13112a39ff5fe34da4703bd4ee0ec299a36098
Chris Dzombak <chris@chrisdzombak.net>
2013-10-28 01:38:26 -0400

    Compatibility with tel:, sms:, smsto: QR codes

--- QRScanner/CDZScanActionText.m
+++ QRScanner/CDZScanActionText.m
@@ -17,13 +17,25 @@
 @implementation CDZScanActionText
 
 + (CDZScanAction *)actionForString:(NSString *)string {
-    NSDataDetector *detector = [NSDataDetector dataDetectorWithTypes:NSTextCheckingTypePhoneNumber error:nil];
-    NSTextCheckingResult *result = [detector firstMatchInString:string options:0 range:NSMakeRange(0, string.length)];
+    NSString *match;
 
-    if (!result) return nil;
+    NSString *firstFour;
+    if (string.length > 4) firstFour = [[string substringWithRange:NSMakeRange(0, 4)] lowercaseString];
+    if ([firstFour isEqualToString:@"tel:"] || [firstFour isEqualToString:@"sms:"]) {
+        match = [string substringFromIndex:4];
+    } else if (string.length > 6 && [[[string substringWithRange:NSMakeRange(0, 6)] lowercaseString] isEqualToString:@"smsto:"]) {
+        match = [string substringFromIndex:6];
+    } else {
+        NSDataDetector *detector = [NSDataDetector dataDetectorWithTypes:NSTextCheckingTypePhoneNumber error:nil];
+        NSTextCheckingResult *result = [detector firstMatchInString:string options:0 range:NSMakeRange(0, string.length)];
+
+        if (!result) return nil;
+
+        match = result.phoneNumber;
+    }
 
     CDZScanActionText *action = [[[self class] alloc] init];
-    action.match = result.phoneNumber;
+    action.match = match;
     return action;
 }
 
